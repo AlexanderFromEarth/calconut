dfb848576578de695c3e6ceb8a82af1d098c1b26
Alexander Andreeff <alexanderfromearth@gmail.com>
2021-08-09 02:00:53 +0500

    Add curry test

---
+++ tests/curry.test.ts
@@ -0,0 +1,15 @@
+describe('Curry', () => {
+  const sumOfThree = (a: number, b: number, c: number) => a + b + c;
+  const curriedSumOfThree = curry(sumOfThree);
+
+  it('f(a)(b)(c)', () => {
+    const result = curriedSumOfThree(2)(3)(4);
+
+    expect(result).toEqual(9);
+  });
+  it('f(a, b, c)', () => {
+    const result = curriedSumOfThree(2, 3, 4);
+
+    expect(result).toEqual(9);
+  });
+});
