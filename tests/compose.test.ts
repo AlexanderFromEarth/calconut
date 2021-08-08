c3feb03d39cf620ffcd6b511ced7a97c602543a7
Alexander Andreeff <alexanderfromearth@gmail.com>
2021-08-09 01:46:32 +0500

    Add compose test

---
+++ tests/compose.test.ts
@@ -0,0 +1,11 @@
+describe('Compose', () => {
+  const addTwo = (num: number) => num + 2;
+  const multiplyByTwo = (num: number) => num * 2;
+
+  it('compose(f, g)', () => {
+    expect(compose(addTwo, multiplyByTwo)(2)).toEqual(addTwo(multiplyByTwo(2)));
+  });
+  it('compose(g, f)', () => {
+    expect(compose(multiplyByTwo, addTwo)(2)).toEqual(multiplyByTwo(addTwo(2)));
+  });
+});
