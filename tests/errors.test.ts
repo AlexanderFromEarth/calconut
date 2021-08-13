ba5b11846d496663d8442bebff08f931eac6b475
Alexander Andreeff <alexanderfromearth@gmail.com>
2021-08-13 23:56:58 +0500

    Add tests for errors

---
+++ tests/errors.test.ts
@@ -0,0 +1,16 @@
+describe('Errors', () => {
+  const successResult = createSuccess(2);
+  const failureResult = createFailure(new CalculationError('Test', null));
+  const addTwo = (res: number) => createSuccess(res + 2);
+
+  it('Then success', () => {
+    const nextSuccess = successResult.then(addTwo) as Success<number>;
+
+    expect(nextSuccess.payload).toEqual(4);
+  });
+  it('Then failure', () => {
+    const nextFailure = failureResult.then(addTwo) as Failure<CalculationError>;
+
+    expect(nextFailure.error.message).toEqual('Test');
+  });
+});
