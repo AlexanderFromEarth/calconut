7572d14313d92b7b319a6b7807f83a645b3d19b4
Alexander Andreeff <alexanderfromearth@gmail.com>
2021-08-16 22:28:33 +0500

    Add test for isTyped

---
+++ tests/isTyped.test.ts
@@ -0,0 +1,8 @@
+describe('IsTyped', () => {
+  it('Typed', () => {
+    expect(isTyped({type: 'TypedObject'})).toEqual(true);
+  });
+  it('Not Typed', () => {
+    expect(isTyped({value: 1})).toEqual(false);
+  });
+});
